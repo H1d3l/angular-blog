02b800e31fe817432aae84e22653391c1f32c4a4
H1d3l <hidels.rios@gmail.com>
2023-09-21 10:39:17 -0300

    content created

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Marvel studios anuncia novo filme do homem de ferro",
+    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "ola mundo",
+    "photoCover":"https://imgs.search.brave.com/sFpFKO4qpvnxU0iQrCZdnCZW_oYxLIfE1vKFqRiQTl8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9maWxl/cy50ZWNub2Jsb2cu/bmV0L3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDIyLzAzL3dhbmRh/LmpwZw"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do pantera negra lançado em breve",
+    "description": "ola mundo",
+    "photoCover":"https://imgs.search.brave.com/sFpFKO4qpvnxU0iQrCZdnCZW_oYxLIfE1vKFqRiQTl8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9maWxl/cy50ZWNub2Jsb2cu/bmV0L3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDIyLzAzL3dhbmRh/LmpwZw"
+  },
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "ola mundo",
+    "photoCover":"https://imgs.search.brave.com/5QJ3K4omQkFjXLBKFB-z_KeeUviBrrw5zNBWHYLb6IQ/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9sdW1p/ZXJlLWEuYWthbWFp/aGQubmV0L3YxL2lt/YWdlcy8wNV94LW1l/bl9jM2Q1OTVmMy5q/cGVnP3JlZ2lvbj0y/NjcsMCwxMDY2LDYw/MA"
+  }
+]
